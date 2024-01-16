3bda5e88fe05fcaf9b55ae514ef851f01c9b3150
MANISH <gyawalimanish12345@gmail.com>
2024-01-16 22:06:13 +0545

    personal portfolio uinclusing home,about, services, skills, education

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".status",{
+    strings:["Computer Engineer ....", "Frontend Developer ...."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop: true
+} )
+
+
+
+
+
